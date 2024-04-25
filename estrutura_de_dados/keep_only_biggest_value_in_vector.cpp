51a6e2d69e1784c3a0f2a63fbff8b70a66bf9097
vit <vitor.roberto3022@gmail.com>
2024-04-24 21:40:27 -0300

    add exercise header

--- estrutura_de_dados/keep_only_biggest_value_in_vector.cpp
+++ estrutura_de_dados/keep_only_biggest_value_in_vector.cpp
@@ -1,3 +1,9 @@
+/*
+Escreva uma função que recebe um vetor de n elementos, determina qual é o maior valor e os demais valores devem ser 
+zerados. Por exemplo, ao receber o vetor v = [ 10, 5, 12, 8], o maior valor encontrado será mantido e os aos demais zerados, 
+desta forma o vetor final será: v = [0, 0, 12, 0]. Esta função deve obedecer ao protótipo abaixo, recebendo como parâmetros o
+número de elementos do vetor e o endereço do vetor. Obs: O Vetor original deve ser alterado.
+*/
 #include <iostream>
 #include <cstdlib> 
 
